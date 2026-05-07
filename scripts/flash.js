2d655af7787b64722cd33bfd3d4c7e229859b264
Josh Tynjala <joshtynjala@bowlerhat.dev>
2026-05-07 10:15:29 -0700

    move flash polyfills into an included script

---
+++ scripts/flash.js
@@ -0,0 +1,46 @@
+var flash = openfl;
+
+flash.utils.getDefinitionByName = function(name)
+{
+	var index = name.lastIndexOf("::");
+	if(index != -1)
+	{
+		name = name.substr(0, index) + "." + name.substr(index + 2);
+	}
+	var def = org.apache.royale.reflection.getDefinitionByName(name);
+	if(def)
+	{
+		return def;
+	}
+	return openfl.Lib.getDefinitionByName(name);
+};
+flash.utils.getQualifiedClassName = function()
+{
+    return org.apache.royale.reflection.getQualifiedClassName(...arguments);
+}
+flash.utils.getQualifiedSuperclassName = function()
+{
+    return org.apache.royale.reflection.getQualifiedSuperclassName(...arguments);
+}
+flash.utils.describeType = function()
+{
+	return new XML();
+}
+
+flash.utils.Proxy = function() {}
+
+flash.utils.Proxy.prototype['http_$$www_adobe_com$2006$actionscript$flash$proxy__getProperty'] = function() {
+    return undefined;
+}
+
+flash.utils.Proxy.prototype['http_$$www_adobe_com$2006$actionscript$flash$proxy__setProperty'] = function() {
+
+}
+
+flash.utils.Proxy.prototype['http_$$www_adobe_com$2006$actionscript$flash$proxy__hasProperty'] = function() {
+    return false;
+}
+
+flash.utils.Proxy.prototype['http_$$www_adobe_com$2006$actionscript$flash$proxy__deleteProperty'] = function() {
+    return false;
+}
